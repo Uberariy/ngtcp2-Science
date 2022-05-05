8a44f2db01a6087f2acc77767503d9da23f383e8
Uberariy <a3.okonishnikov@mail.ru>
2022-05-05 22:56:26 +0300

    Minor fix

--- lib/ngtcp2_bbrfrcst.c
+++ lib/ngtcp2_bbrfrcst.c
@@ -1018,7 +1018,7 @@ static int bbr_check_inflight_too_high(ngtcp2_bbr2_cc *bbr,
 }
 
 static int is_inflight_too_high(ngtcp2_bbr2_cc *bbr, ngtcp2_conn_stat *cstat, const ngtcp2_rs *rs) {
-  bbr->ultra_loss = (bbr->ultra_loss * 255 + (float)rs->lost * NGTCP2_BBR_LOSS_THRESH_DENOM / rs->tx_in_flight) / 256;
+  bbr->ultra_loss = (bbr->ultra_loss * 127 + (float)rs->lost * NGTCP2_BBR_LOSS_THRESH_DENOM / rs->tx_in_flight) / 128;
   ngtcp2_log_info(bbr->ccb.log, NGTCP2_LOG_EVENT_RCV, "bbrfrcst updated ultra_loss=%f", bbr->ultra_loss);
   ngtcp2_log_info(bbr->ccb.log, NGTCP2_LOG_EVENT_RCV,
                   "bbr2 on pckt_loss loss2=%f", 
