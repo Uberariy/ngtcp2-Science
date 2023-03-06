85d7520999e30abfda6de06033b09a40e53190bc
Uberariy <a3.okonishnikov@mail.ru>
2023-03-06 12:26:20 +0300

    merge

--- perf/cong_window_to_perf.py
+++ perf/cong_window_to_perf.py
@@ -143,12 +143,12 @@ if __name__ == "__main__":
 
 # %%
 def explore_cong_window_in_one_dot(df_dot, dot, channel_features):
-    fig, axes = plt.subplots(4, 1, figsize=(8, 17))
-    fig.suptitle('Dependencies between BBR Congestion Window and features in dot: [' + str(dot) + '].')
+    fig, axes = plt.subplots(3, 1, figsize=(9, 13))
+    fig.suptitle(f'Dependencies between BBR Congestion Window and features.\nRTT = {dot[1]} ms, Loss = {dot[2]} %, BW = {dot[3]} Kbit/s.')
     sns.lineplot(ax=axes[0],data=df_dot, x="Preset Congestion Window (bytes)", y=f"Sender Speed (Kbit/s)", ci=70)
     sns.lineplot(ax=axes[1],data=df_dot, x="Preset Congestion Window (bytes)", y=f"Sender lost data to data sent (%)", ci=70)
     sns.lineplot(ax=axes[2],data=df_dot, x="Preset Congestion Window (bytes)", y=f"Sender RTT (ms)", ci=70)
-    sns.lineplot(ax=axes[3],data=df_dot, x="Preset Congestion Window (bytes)", y=f"Experiment duration (sec)", ci=70)
+    # sns.lineplot(ax=axes[3],data=df_dot, x="Preset Congestion Window (bytes)", y=f"Experiment duration (sec)", ci=70)
 
 if __name__ == "__main__":
     channel_features = ["Congestion Controller", "Channel RTT (ms)", 'Channel Loss (%)', "Channel BW (Kbit/s)", "Channel Jitter (ms)"]
@@ -165,4 +165,6 @@ if __name__ == "__main__":
 
 # %%
 df.loc[df["Sender lost data to data sent (%)"] > 100]
+# %%
+df
 # %%
